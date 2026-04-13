70812fb6564e223ffe778f4b980390d6b02fd2af
min12-proton-me <min20111005@proton.me>
2026-04-13 17:58:10 +0900

    6698 lines (5600 loc) · 213 KB

--- taolao.lua
+++ taolao.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 if getgenv().celina_Running then
     warn("Script already running!")
     return
